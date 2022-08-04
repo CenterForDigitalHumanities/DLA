4694192b573173307ef8001ba377be08c85cc6b6
Patrick Cuba <cubap@slu.edu>
2022-08-04 10:31:48 -0500

    messed up tooltips

--- public/javascripts/deer/components/view/collection.js
+++ public/javascripts/deer/components/view/collection.js
@@ -6,16 +6,223 @@ let progress
 
 const template = obj => {
 // Full URIs can also be used, but the internal ids are a bit more readable and bookmarkable and stubbable.
-    return `<div><header>${obj.name}</header>
+    return `
+    <style scoped>
+        .flex-reverse {
+        box-sizing: border-box;
+        max-width: var(--grid-maxWidth);
+        margin: 0 auto;
+        width: 96%;
+        padding: 0 calc(var(--grid-gutter) / 2);
+        display: flex;
+        flex-direction: column-reverse;
+        }
+
+        .row {
+            display: flex;
+            flex-flow: row wrap;
+            justify-content: flex-start;
+            margin-left: calc(var(--grid-gutter) / -2);
+            margin-right: calc(var(--grid-gutter) / -2);
+            flex-wrap: initial;
+        }
+
+        .col {
+            -webkit-box-flex: 1;
+            -ms-flex: 1;
+            flex: 1;
+        }
+
+        .col,
+        [class*=" col-"],
+        [class^='col-'] {
+            margin: 0 calc(var(--grid-gutter) / 2) calc(var(--grid-gutter) / 2);
+        }
+
+        a.col {
+            max-width: 40vw;
+        }
+
+        a.col img {
+            object-fit: contain;
+            object-position: center;
+            max-width: 100%;
+        }
+
+        p img {
+            width: 100%;
+        }
+
+        .sidebar {
+            max-width: 30em;
+            min-width: 200px;
+            flex-basis: 200px;
+            position: relative;
+            flex-grow: 2;
+        }
+
+        .sidebar ul {
+            margin: 0;
+            padding: 0;
+        }
+
+        .sidebar li {
+            padding: calc(var(--grid-gutter) / 10) 0;
+            margin: 0;
+            display: block;
+            overflow: hidden;
+            text-transform: capitalize;
+            text-overflow: ellipsis;
+            background: var(--site-dark);
+            color: var(--site-light);
+            border-left: var(--site-dark) solid thick;
+        }
+
+        a {
+            text-decoration: none;
+            font-weight: 400;
+            color: var(--color-link);
+            transition: color 0.5s ease;
+        }
+
+        a:hover,
+        a:focus {
+            color: var(--action);
+            text-decoration: none !important;
+            cursor: pointer;
+        }
+
+        facet {
+            display: block;
+            position: relative;
+            cursor: pointer;
+            transition: background-color .2s;
+            border-left: var(--accent) solid thin;
+            padding-left: calc(var(--grid-gutter)/4);
+            font-size: smaller;
+        }
+
+        facet:hover {
+            background-color: var(--accent);
+        }
+
+        facet.clicked {
+            background-color: var(--site-light);
+            color: var(--font-color);
+        }
+
+        facet::after {
+            content: "(" attr(data-count)")";
+            position: absolute;
+            right: 0;
+            border-radius: 15%;
+            background-color: rgba(255, 255, 255, .5);
+        }
+
+        .sidebar progress,
+        .sidebar input {
+            box-sizing: border-box;
+            width: 100%;
+            transition: width, progress-value .5s ease-out;
+        }
+
+        .sidebar input {
+            line-height: 1.6;
+            margin-bottom: var(--grid-gutter);
+        }
+
+        progress::after {
+            content: attr(value)" of " attr(max);
+            font-size: .6em;
+            color: #666666;
+            position: relative;
+            text-align: right;
+            display: block;
+            bottom: 1em;
+        }
+
+        .grow {
+            flex-grow: 6;
+        }
+
+        .wrap {
+            flex-wrap: wrap;
+            display: flex;
+        }
+
+        .thumbnail {
+            object-fit: cover;
+            object-position: top center;
+            padding: calc(var(--grid-gutter) / 2);
+            max-height: 10em;
+            min-width: 5em;
+        }
+
+        .record {
+            display: flex;
+            flex-basis: 100%;
+            flex-direction: column;
+            padding: calc(var(--grid-gutter) / 2);
+            box-shadow: -1px -1px 3px rgba(0, 0, 0, .4);
+            margin: calc(var(--grid-gutter) / 4);
+        }
+
+        .record[class*='hide-'],
+        facet[class*='hide-'] {
+            display: none !important;
+        }
+
+        dt,
+        dd {
+            border-top: 1px solid var(--bg-secondary-color);
+            padding-right: 0;
+            line-height: 1.42857143;
+            margin: 0 auto;
+            color: var(--font-color);
+            display: inline-block;
+            padding-top: 6px;
+            padding-bottom: 4px;
+            box-sizing: border-box;
+            vertical-align: top;
+        }
+
+        dt {
+            font-weight: 700;
+            width: 15%;
+        }
+
+        dd {
+            width: 85%;
+            word-break: break-word;
+            padding-left: 40px;
+        }
+
+        dd p {
+            padding: 0;
+            margin: 0;
+        }
+
+        .record h4 {
+            margin: 0 auto;
+            font-size: 1.3em;
+            padding: 0.5em 0em;
+        }
+
+        #Records {
+            overflow: auto;
+            height: calc(100vh - 75px);
+        }
+    </style>
+    <div><header>${obj.name}</header>
         <span class="badge">${obj.numberOfItems??``}</span> ${obj.description??``}
-        <div class="flex-reverse">
-        <div id="Records" class="grow wrap">list loading</div>
-        <div class="sidebar">
+        <div class="flex-container">
+            <div class="sidebar">
                 <h3>Refine Results <button role="button" id="queryReset">clear all</button></h3>
                 <progress value="${obj.numberOfItems??0}" max="${obj.numberOfItems??10}">${obj.numberOfItems??``} of ${obj.numberOfItems??``}</progress>
                 <input id="query" type="text" placeholder="type to filter">
                 <section id="facetFilter"></section>
             </div>
+            <div id="Records" class="grow wrap">list loading</div>
         </div>
     </div>`
 }
@@ -48,7 +255,7 @@ export default class DLA_Collection extends DeerView {
     }
 
     async #loadRecords(pagination = [0,]) {
-        const data = await this.#fetchList(this.getAttribute(`${DEER.PREFIX}-id`)).catch(err => flashMessage(err)) ?? {}
+        const data = await this.#fetchList(this.getAttribute(`${DEER.PREFIX}-id`)).catch(err => this.#flashMessage(err)) ?? {}
         return (data[this.getAttribute(`${DEER.PREFIX}-list`) ?? "itemListElement"] ?? []).slice(pagination[0], pagination[1])
     }
 
