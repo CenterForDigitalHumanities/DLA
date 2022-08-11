c01a8d6955213d69e54bec1958b2ec009a18b6e8
Patrick Cuba <cubap@slu.edu>
2022-08-11 12:54:44 -0500

    Including deer (#15)
    * importing deer
    
    forked from the v1.0 candidate
    
    * why is /collection/:collectionID
    
    failing when I use sendFile()
    
    * view and review were sitting in a boat
    
    * simplify the templates
    
    * migrating collections into DLA from Newberry
    
    * search css
    
    * filtering
    
    CSS is still weird on the reverse flex
    
    * messed up tooltips
    
    * record view
    
    * better
    
    * options for batches
    
    * only announce with changes
    
    * Update entities.js
    
    * PR cleanup
    
    fixes #18
    fixes #19
    
    * more for #18
    
    collection template redraws when data is recevied
    
    * setting a dumb floor for #17
    
    simplest 404 error handling.
    fixes #17
    
    * adding a 500 case
    
    * ranming const variable for consistency
    
    Co-authored-by: Bryan Haberberger <bryan.j.haberberger@slu.edu>

--- app.js
+++ app.js
@@ -1,43 +1,45 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const dotenv = require('dotenv');
-dotenv.config();
+var createError = require('http-errors')
+var express = require('express')
+var path = require('path')
+var cookieParser = require('cookie-parser')
+var logger = require('morgan')
+const dotenv = require('dotenv')
+dotenv.config()
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var managementRouter = require('./routes/management');
-var collectionsRouter = require('./routes/collections');
+var indexRouter = require('./routes/index')
+var usersRouter = require('./routes/users')
+var managementRouter = require('./routes/management')
+var collectionsRouter = require('./routes/collections')
+var collectionRouter = require('./routes/collection')
 
-var app = express();
+var app = express()
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/management', managementRouter);
-app.use('/collections', collectionsRouter);
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', indexRouter)
+app.use('/users', usersRouter)
+app.use('/management', managementRouter)
+app.use('/collections', collectionsRouter)
+app.use('/collection', collectionRouter)
+app.use(express.static(path.join(__dirname, 'public')))
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
-});
+  next(createError(404))
+})
 
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
 
   // render the error page
-  res.status(err.status || 500);
-  res.redirect('/error.html');
-});
+  res.status(err.status || 500)
+  res.redirect('/error.html')
+})
 
-module.exports = app;
+module.exports = app
