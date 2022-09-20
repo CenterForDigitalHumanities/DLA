d2222df897e1ba56151a56ae5770351dbb882a16
Patrick Cuba <cubap@slu.edu>
2022-09-20 15:29:01 -0500

    Revert "Including deer (#15)"
    This reverts commit c01a8d6955213d69e54bec1958b2ec009a18b6e8.

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,9 @@
-var express = require('express')
-var router = express.Router()
+var express = require('express');
+var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send({user:"username",id:12})
-})
+  res.send({user:"username",id:12});
+});
 
-module.exports = router
+module.exports = router;
