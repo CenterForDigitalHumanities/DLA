04df49c3f1798f3e9cc8d9098cc847b49053d2d5
Patrick Cuba <cubap@slu.edu>
2022-09-29 00:02:17 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const dotenv = require('dotenv');
-dotenv.config();
+var createError = require('http-errors')
+var express = require('express')
+var path = require('path')
+var cookieParser = require('cookie-parser')
+var logger = require('morgan')
+const dotenv = require('dotenv')
+dotenv.config()
 
 var indexRouter = require('./routes/index')
 var usersRouter = require('./routes/users')
@@ -13,12 +13,12 @@ var collectionsRouter = require('./routes/collections')
 var collectionRouter = require('./routes/collection')
 var poemRouter = require('./routes/poem')
 
-var app = express();
+var app = express()
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
 
 app.use('/', indexRouter)
 app.use('/users', usersRouter)
@@ -30,18 +30,18 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
-});
+  next(createError(404))
+})
 
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
 
   // render the error page
-  res.status(err.status || 500);
-  res.redirect('/error.html');
-});
+  res.status(err.status || 500)
+  res.redirect('/error.html')
+})
 
-module.exports = app;
+module.exports = app
