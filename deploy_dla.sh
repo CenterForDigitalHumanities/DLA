a75a151f2e3364e4d8f35ecde11b194947dbe72c
Bryan Haberberger <bryan.j.haberberger@slu.edu>
2023-01-30 13:47:30 -0600

    attempt a CD

--- deploy_dla.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [[ ! -e /opt/nodeapps/logs/dlalogs.txt ]]; then
-    mkdir -p /opt/nodeapps/logs
-    touch /opt/nodeapps/logs/dlalogs.txt
-fi
-cd /opt/nodeapps/dla-public-site-3001
-npm install
-npm start >> /opt/nodeapps/logs/dlalogs.txt 2>&1 &
